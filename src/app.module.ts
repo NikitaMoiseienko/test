3fe8ddc295c714212b7bb80e3766e671f64a0e04
matmoim <matmoiseenko@gmail.com>
2021-07-09 19:29:53 +0300

    cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { Cat } from './cats/cats.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'mathew',
+      password: 'qwerty',
+      database: 'warehouse',
+      entities: [Cat],
+      synchronize: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
