c229b3c948fbdcdbc54a0d772c32000673544590
matmoim <matmoiseenko@gmail.com>
2021-07-12 00:02:36 +0300

    create entity

--- src/components/item/entities/item.entity.ts
+++ src/components/item/entities/item.entity.ts
@@ -1,1 +1,23 @@
-export class Item {}
+import { Shop } from 'src/components/shop/entities/shop.entity';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column({ default: false })
+  isPreviouslyUsed: boolean;
+
+  @Column({ enum: ['black', 'white', 'pink', 'red'] })
+  color: string;
+
+  @ManyToMany(() => Shop, (shop) => shop.items)
+  shops: Shop[];
+}
--- src/components/shop/entities/shop.entity.ts
+++ src/components/shop/entities/shop.entity.ts
@@ -1,1 +1,28 @@
-export class Shop {}
+import { Item } from 'src/components/item/entities/item.entity';
+import { Column, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Shop {
+  @ManyToMany(() => Item, (item) => item.shops)
+  items: Item[];
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, unique: true })
+  name: string;
+
+  @Column()
+  adress: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  workTime: string;
+
+  @Column({ default: false })
+  isOnline: boolean;
+
+  @Column({ default: false })
+  delivery: boolean;
+}
